bb1f257c425b68e9d42517c38098d0f3f1ecf30d
gokhangnccn <gokhangnccn@gmail.com>
2024-09-03 15:09:34 +0300

    init project

---
+++ src/main/java/com/dicore/fatura_yonetim_sistemi/FaturaYonetimSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.dicore.fatura_yonetim_sistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaYonetimSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaYonetimSistemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicore/fatura_yonetim_sistemi/FaturaYonetimSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicore.fatura_yonetim_sistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturaYonetimSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
